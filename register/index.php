bc45c6d2a59afbc2f377b14da69e343403ddc5aa
haidnvmo <haidn@vmodev.com>
2021-07-04 10:19:19 +0700

    admin

---
+++ register/index.php
@@ -0,0 +1,23 @@
+<?php
+    include("../layout/hearder.php");
+?>
+
+    <div class="form-group">
+      <label for="">Email</label>
+      <input type="text" class="form-control" name="email" id="" aria-describedby="emailHelpId" placeholder="">
+    </div>
+    <div class="form-group">
+      <label for="">Name</label>
+      <input type="text" class="form-control" name="name" id="" aria-describedby="emailHelpId" placeholder="">
+    </div>
+    <div class="form-group">
+      <label for="">Password</label>
+      <input type="password" class="form-control" name="password" id="" aria-describedby="emailHelpId" placeholder="">
+    </div>
+    <div class="form-group">
+      <label for="">ConfirmPassword</label>
+      <input type="password" class="form-control" name="confirmpassword" id="" aria-describedby="emailHelpId" placeholder="">
+    </div>
+<?php
+    include("../layout/footer.php");
+?>
